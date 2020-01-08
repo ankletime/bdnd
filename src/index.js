8103e6c219d99280f8816d8f2ccd389e49237071
Yakov <ysall@zreiss.com>
2020-01-08 17:00:28 -0500

    Dnd

--- src/index.js
+++ src/index.js
@@ -1,12 +1,63 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import initalData from "./intial-data";
+import Column from "./column";
+import "@atlaskit/css-reset";
+import { DragDropContext } from "react-beautiful-dnd";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends Component {
+  state = initalData;
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  onDragEnd = result => {
+    console.log(result);
+    const { destination, source, draggableId } = result;
+
+    if (!destination) {
+      return;
+    }
+
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+
+    const column = this.state.columns[source.droppableId];
+    const newTaskIds = Array.from(column.taskIds);
+    newTaskIds.splice(source.index, 1);
+    newTaskIds.splice(destination.index, 0, draggableId);
+
+    console.log(newTaskIds);
+
+    const newColumn = {
+      ...column,
+      taskIds: newTaskIds
+    };
+
+    console.log(newColumn);
+
+    const newState = {
+      ...this.state,
+      columns: {
+        ...this.state.columns,
+        [newColumn.id]: newColumn
+      }
+    };
+
+    this.setState(newState);
+  }
+  render() {
+    return (
+      <DragDropContext onDragEnd={this.onDragEnd}>
+        {this.state.columnOrder.map(columnId => {
+          const column = this.state.columns[columnId];
+          const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
+
+          return <Column key={column.id} column={column} tasks={tasks} />;
+        })}
+      </DragDropContext>
+    );
+  }
+}
+ReactDOM.render(<App />, document.getElementById("root"));
